abb938a5968958e64e15612f8060744f08ae4a1c
wndms12 <70639486+wndms12@users.noreply.github.com>
2020-09-23 16:27:14 +0900

    Add files via upload

---
+++ aa2-19-rpt04/sketch02_pwm_led.ino
@@ -0,0 +1,20 @@
+
+int pwm = 0;
+int led = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pwm = random(0,255);
+  pwmLed(led,pwm);
+}
+
+void pwmLed(int led, int pwmValue){
+
+  analogWrite(led, pwmValue);
+  delay(100);
+}
---
+++ aa2-19-rpt04/sketch03_pwm_led_serial.ino
@@ -0,0 +1,24 @@
+
+int pwm = 0;
+int led = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pwm = random(0,255);
+  pwmLed(led,pwm);
+
+  Serial.print("AA15,AA19, LED: ");
+  Serial.println(pwm);
+  delay(10);
+}
+
+void pwmLed(int led, int pwmValue){
+
+  analogWrite(led, pwmValue);
+  delay(100);
+}
